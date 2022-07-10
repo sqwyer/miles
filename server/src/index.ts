e7d7843e5afea71baf3f3911ed78e6fa120c6629
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-07-10 13:08:07 -0500

    Create build step and config ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
-import express, {Request, Response} from 'express'
+import * as ex from 'express'
 
 const PORT = process.env.PORT || 3000
-const app = express()
+const app = ex()
 const cwd = process.cwd()
+
+app.use('/static', ex.static(`${cwd}/client/static`))
 
 app.set('view engine', 'hbs')
 app.set('views', `${cwd}/client/pages`)
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/', (_req: ex.Request, res: ex.Response) => {
     res.render('index')
 })
 
